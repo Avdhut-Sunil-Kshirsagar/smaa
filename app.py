549a422ae9015a72401166c00c48d8f2bd135bef
Avdhut Sunil Kshirsagar <151182840+Avdhut-Sunil-Kshirsagar@users.noreply.github.com>
2025-04-28 17:25:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ MODEL_DIR = os.path.join(os.getenv('MODEL_DIR', '/tmp/model'))
 MODEL_PATH = os.path.join(MODEL_DIR, "final_model_11_4_2025.keras")
 TARGET_SIZE = (224, 224)
 CLASS_NAMES = ['AI', 'FAKE', 'REAL']
-MAX_FILE_SIZE = 50 * 1024 * 1024  # 10MB
+MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB
 MAX_WORKERS = 2  # Limit concurrent processing for CPU constraints
 
 # Pydantic Models
