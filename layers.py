0c5e4c43b9d20ddccd03a8f6b57dc7627cd722df
Avdhut Sunil Kshirsagar <151182840+Avdhut-Sunil-Kshirsagar@users.noreply.github.com>
2025-04-30 17:24:28 +0530

    Create layers.py

---
+++ layers.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+class EfficientChannelAttention(layers.Layer):
+    def __init__(self, channels, reduction=8, **kwargs):
+        super().__init__(**kwargs)
+        self.channels = channels
+        self.reduction = reduction
+        self.avg_pool = layers.GlobalAveragePooling2D()
+        self.max_pool = layers.GlobalMaxPooling2D()
+        self.fc = models.Sequential([
+            layers.Dense(channels//reduction, activation='relu'),
+            layers.Dense(channels, activation='sigmoid')
+        ])
+        
+    def call(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return tf.reshape(out, [-1, 1, 1, self.channels]) * x
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update({'channels': self.channels, 'reduction': self.reduction})
+        return config
+
+class FixedSpatialAttention(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.conv = layers.Conv2D(1, 7, padding='same', activation='sigmoid')
+        
+    def call(self, inputs):
+        avg_out = tf.reduce_mean(inputs, axis=3, keepdims=True)
+        max_out = tf.reduce_max(inputs, axis=3, keepdims=True)
+        concat = tf.concat([avg_out, max_out], axis=3)
+        attention = self.conv(concat)
+        return attention * inputs
+    
+    def get_config(self):
+        return super().get_config()
+
+class FixedHybridBlock(layers.Layer):
+    def __init__(self, filters, **kwargs):
+        super().__init__(**kwargs)
+        self.filters = filters
+        self.conv1 = layers.Conv2D(filters, 3, padding='same')
+        self.conv2 = layers.Conv2D(filters, 3, padding='same')
+        self.eca = EfficientChannelAttention(filters)
+        self.sa = FixedSpatialAttention()
+        self.norm1 = layers.BatchNormalization()
+        self.norm2 = layers.BatchNormalization()
+        self.act =
